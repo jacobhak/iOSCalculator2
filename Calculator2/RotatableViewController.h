6dc138436099f2de316d5e368aa2d58fac317dc3
jacobhak <jacobhakansson@gmail.com>
2012-12-10 20:42:47 +0100

    Assignment 3 almost finished

---
+++ Calculator2/RotatableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RotatableViewController.h
+//  Calculator2
+//
+//  Created by Jacob Håkansson on 2012-12-10.
+//  Copyright (c) 2012 Jacob Håkansson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotatableViewController : UIViewController
+
+@end
---
+++ Calculator2/RotatableViewController.m
@@ -0,0 +1,38 @@
+//
+//  RotatableViewController.m
+//  Calculator2
+//
+//  Created by Jacob Håkansson on 2012-12-10.
+//  Copyright (c) 2012 Jacob Håkansson. All rights reserved.
+//
+
+#import "RotatableViewController.h"
+
+@interface RotatableViewController ()
+
+@end
+
+@implementation RotatableViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
